5f3f784d6909facf99d31045b8252c062741763c
Gaurav Bhardwaj <smartgaurav22@gmail.com>
2020-07-17 23:41:10 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,61 @@
+'''
+ASSIGNMENT CHECKPOINT-1 (ITTWIST)
+Mimic a simple blog commandline program:
+
+Suppose you are planning to create your own blog application. Where you can read a posts,
+create a post, update a post or delete a post.
+
+1. Create a list (posts) that will contain all post (dictionaries).
+2. A post dictionary will contain following attributes:
+  * a post id (post_id)
+  * a post name (post_name)
+  * a post author (post_author)
+3. Create a following functions:
+  * read_post which will print the posts list.
+  * create_post which will accept a arbitrary keyword arguments, and push a dictionary
+    in the posts list.
+  * update_post which will accept a arbitrary keyword arguments, and update a dictionary
+    of the respective post_id.
+  * delete_post which will accept post_id as a argument, and delete that dictionary of the
+    respective post_id.
+4. Call the following function
+   create_post
+   create_post
+   create_post
+   read_post
+   update_post
+   read_post
+   delete_post
+'''
+
+
+posts = []
+
+#Create a post, Please change the parameter list accordingly
+def create_post():
+    pass
+
+#Update a specific post, Please change the parameter list accordingly
+def update_post():
+    pass
+
+#Read all post, Please add the print statement for updating the posts
+def read_post():
+    pass
+
+#Delete a specific post, Please change the parameter list accordingly
+def delete_post():
+    pass
+
+#This will call the create post function, Please change the argument list accordingly
+create_post()
+create_post()
+create_post()
+#This will call the read post function, Please change the argument list accordingly
+read_post()
+#This will call the update post function, Please change the argument list accordingly
+update_post()
+read_post()
+#This will call the delete post function, Please change the argument list accordingly
+delete_post()
+read_post()
