f5119d243e2c093e860dcc5d6bfa97b11d448e3a
Gaurav Bhardwaj <smartgaurav22@gmail.com>
2020-07-18 13:26:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,11 @@
 ASSIGNMENT CHECKPOINT-1 (ITTWIST)
 Mimic a simple blog commandline program:
 
-Suppose you are planning to create your own blog application. Where you can read a posts,
+Suppose you are planning to create your own blog application. Where you can read a post,
 create a post, update a post or delete a post.
 
 1. Create a list (posts) that will contain all post (dictionaries).
-2. A post dictionary will contain following attributes:
+2. A post dictionary will contain following attributes, please refer to function calls:
   * a post id (post_id)
   * a post name (post_name)
   * a post author (post_author)
@@ -18,7 +18,7 @@ create a post, update a post or delete a post.
     of the respective post_id.
   * delete_post which will accept post_id as a argument, and delete that dictionary of the
     respective post_id.
-4. Call the following function
+4. The following function calls are already written for you, please call them respectively and verify the expected output.
    create_post
    create_post
    create_post
@@ -49,28 +49,34 @@ def delete_post():
     pass
 
 #This will call the create post function
-create_post()
-create_post()
-create_post()
+create_post(post_id=1,post_name='Python Course Part-1',post_author='John')
+create_post(post_id=2,post_name='Python Course Part-2',post_author='Bill')
+create_post(post_id=3,post_name='Python Course Part-3',post_author='Jack')
 #This will call the read post function
 read_post()
 '''Expected Output : 
-[{'post_id': 1, 'post_name': 'Python Course Part-1', 'post_author': 'John'}, 
+[
+{'post_id': 1, 'post_name': 'Python Course Part-1', 'post_author': 'John'}, 
 {'post_id': 2, 'post_name': 'Python Course Part-2', 'post_author': 'Bill'}, 
-{'post_id': 3, 'post_name': 'Python Course Part-3', 'post_author': 'Jack'}]
+{'post_id': 3, 'post_name': 'Python Course Part-3', 'post_author': 'Jack'}
+]
 '''
 #This will call the update post function
-update_post()
+update_post(post_id=1,post_name='Python Course Part-0',author='Max')
 read_post()
 ''' Expected Output:
-[{'post_id': 1, 'post_name': 'Python Course Part-0', 'post_author': 'Max'}, 
+[
+{'post_id': 1, 'post_name': 'Python Course Part-0', 'post_author': 'Max'}, 
 {'post_id': 2, 'post_name': 'Python Course Part-2', 'post_author': 'Bill'}, 
-{'post_id': 3, 'post_name': 'Python Course Part-3', 'post_author': 'Jack'}]
+{'post_id': 3, 'post_name': 'Python Course Part-3', 'post_author': 'Jack'}
+]
 '''
 #This will call the delete post function
 delete_post()
 read_post()
 ''' Expected Output:
-[{'post_id': 1, 'post_name': 'Python Course Part-0', 'post_author': 'Max'}, 
-{'post_id': 2, 'post_name': 'Python Course Part-2', 'post_author': 'Bill'}]
+[
+{'post_id': 1, 'post_name': 'Python Course Part-0', 'post_author': 'Max'}, 
+{'post_id': 2, 'post_name': 'Python Course Part-2', 'post_author': 'Bill'}
+]
 '''
